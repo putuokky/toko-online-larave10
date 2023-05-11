688a2f1291ef3d138710d27f6ab1cb831eeb35ed
putuokky <okkymahes@gmail.com>
2023-05-11 15:23:04 +0800

    Membuat Toko Online dengan Laravel 10 - #5 Tinker, Factory, dan Database Seeder done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Modules\Shop\Database\Seeders\ShopDatabaseSeeder;
 
@@ -20,6 +22,16 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call(ShopDatabaseSeeder::class);
+        if ($this->command->confirm('Do you want to refresh migration before seeding, it will clear all old data ?')) {
+            $this->command->call('migrate:refresh');
+            $this->command->info('Data cleared, starting from blank database');
+        }
+
+        User::factory()->create();
+        $this->command->info('sample user seeded.');
+
+        if ($this->command->confirm('Do you want to seed some sample products  ?')) {
+            $this->call(ShopDatabaseSeeder::class);
+        }
     }
 }
